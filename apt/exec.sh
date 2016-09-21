4f0a0f0efa2b08fae9cebd1a9a49f2909a31cbc6
root <root@debian>
2016-09-20 17:54:20 -0700

    update commands

--- apt/exec.sh
+++ apt/exec.sh
@@ -4,3 +4,5 @@ done <InstallPackages.txt
 #cat ..txt | xargs apt-get install -y
 #vim - replace "install" words
 #dpkg list all installed packages
+#dpkg --get-selections | grep -v deinstall > EnvirSetting/apt/InstallPackages.txt
+#tr -d "\t" < InstallPackages.txt  >& log
