f2a88b3d4be2b98c3c8d00cabd104f0a7b8dfeb6
Ubuntu <ubuntu@ip-172-31-9-156.us-west-2.compute.internal>
2017-03-18 03:59:19 +0000

    AWS

---
+++ apt/general/exec.sh
@@ -0,0 +1,9 @@
+apt-get update
+while read p; do
+  apt-get install -y $p 
+done <InstallPackages.txt
+#cat ..txt | xargs apt-get install -y
+#vim - replace "install" words
+#dpkg list all installed packages
+#dpkg --get-selections | grep -v deinstall > EnvirSetting/apt/InstallPackages.txt
+#tr -d "\t" < InstallPackages.txt  >& log
