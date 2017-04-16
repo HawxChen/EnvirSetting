e7e34c783b3fc0314172d4c65973e88b03fb97e5
HawxChen <yingjheng.chen@gmail.com>
2017-04-16 04:17:41 +0000

    done

--- aws/ubuntu1604/cluster/sbin/reboot_all.sh
+++ aws/ubuntu1604/cluster/sbin/reboot_all.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 ./cls_cmd.sh "sudo shutdown -r now"
+sudo shutdown -r now
--- aws/ubuntu1604/cluster/sbin/start_all_dds.sh
+++ aws/ubuntu1604/cluster/sbin/start_all_dds.sh
@@ -2,7 +2,8 @@
 set -o xtarce
 eval `ssh-agent`
 ssh-add DISTRIUBTE_COPUTING.pem
-sudo rm -rf /tmp/hadoop*
+sudo rm -rf /tmp/hadoo* /tmp/hsper* /tmp/Jetty*
+./cls_cmd.sh "sudo rm -rf /tmp/hadoo* /tmp/hsper* /tmp/Jetty*"
 hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
