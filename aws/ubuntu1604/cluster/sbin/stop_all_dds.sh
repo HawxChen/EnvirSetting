4f200714d0a4bd2203a9eacaab3915609f5953cc
HawxChen <yingjheng.chen@gmail.com>
2017-04-17 16:23:49 +0000

    shell script

--- aws/ubuntu1604/cluster/sbin/stop_all_dds.sh
+++ aws/ubuntu1604/cluster/sbin/stop_all_dds.sh
@@ -9,4 +9,6 @@ $SPARK_HOME/sbin/stop-master.sh
 $HADOOP_HOME/sbin/stop-yarn.sh
 $HADOOP_HOME/sbin/stop-dfs.sh
 sudo rm -rf /tmp/hadoop*
+sudo rm -rf /tmp/hadoo* /tmp/hsper* /tmp/Jetty*
+./cls_cmd.sh "sudo rm -rf /tmp/hadoo* /tmp/hsper* /tmp/Jetty*"
 pkill ssh-agent
